bff62e083fac02402b9bca37c2232f895dce61ce
Deirdre Saoirse Moen <deirdre@deirdre.net>
2023-02-03 22:38:49 -0800

    Previews were crashing on an M1 Max Mac running Ventura 13.1.
    1. Migrated the preview track creation into a Preview Content extension.
    2. Updated the PreviewProvider to use the extension.

---
+++ SwiftUI/project7/FastTrack/Preview Content/Track+Preview.swift
@@ -0,0 +1,14 @@
+//
+//  Track+Preview.swift
+//  Project 7 Fast Track
+//
+//  Created by Deirdre Saoirse Moen on 1/31/23.
+//
+
+import Foundation
+
+extension Track {
+	static var track1: Track {
+		Track(trackId: 1, artistName: "Nirvana", trackName: "Smells Like Teen Spirit", previewUrl: URL(string: "abc")!, artworkUrl100: "https://bit.ly/teen-spirit")
+	}
+}
